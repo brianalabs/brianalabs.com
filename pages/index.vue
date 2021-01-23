c95e652812366132aeb172ce936729d1bb9486ef
peterkimzz <peterkimzz69@gmail.com>
2021-01-23 23:22:33 +0900

    hero done

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <header :class="['bg-pattern', 'py-20 md:py-24']">
+    <header :class="['bg-pattern', 'pt-20 pb-10 md:py-24']">
       <vue-container>
-        <div :class="['relative', 'pb-44']">
+        <div :class="['relative', 'pb-48']">
           <h1
             :class="[
               'font-extrabold',
@@ -18,29 +18,64 @@
             >
           </h1>
 
-          <p
-            :class="[
-              'text-gray-400',
-              'text-sm md:text-base lg:text-lg',
-              'pt-4 md:pt-6 lg:pt-8',
-              'max-w-xs md:max-w-sm lg:max-w-lg'
-            ]"
-          >
-            브리아나는 당신의 성공적인
-            <strong class="font-semibold text-gray-200">코인 투자</strong>를
-            위한 인공지능 비서입니다. 머신러닝, 딥러닝으로 학습하는 브리아나의
-            강력한 기능을 무료로 사용해보세요.
-          </p>
+          <div :class="['text-sm md:text-base lg:text-lg']">
+            <div
+              :class="[
+                'pt-4 md:pt-6 lg:pt-8',
+                'max-w-xs md:max-w-md lg:max-w-lg'
+              ]"
+            >
+              <p :class="['text-gray-400']">
+                브리아나는 당신의 성공적인
+                <b class="font-semibold text-gray-200">코인 투자</b>를 위한
+                인공지능 비서입니다. 머신러닝, 딥러닝으로 학습하는 브리아나의
+                강력한 기능을 무료로 사용해보세요.
+              </p>
+            </div>
 
-          <a
-            :class="[
-              'inline-block rounded-lg font-semibold',
-              'bg-primary-600 text-primary-100',
-              'mt-10',
-              'py-3 px-6'
-            ]"
-            >브리아나 랩스의 첫 번째 서비스 "스토커"가 곧 출시됩니다!</a
-          >
+            <div
+              :class="[
+                'inline-flex flex-col items-center',
+                'w-full md:w-auto',
+                'mt-6 md:mt-10'
+              ]"
+            >
+              <a
+                :class="[
+                  'inline-block rounded-lg font-semibold',
+                  'bg-primary-800 text-primary-300',
+                  'py-3 px-6',
+                  'text-xs md:text-sm'
+                ]"
+                >브리아나 랩스의 첫 번째 서비스
+                <strong class="text-primary-100 font-bold">스토커</strong>가 곧
+                출시됩니다!</a
+              >
+              <button
+                :class="[
+                  'inline-flex items-center',
+                  'mt-3',
+                  'text-gray-400',
+                  'text-xs',
+                  'hover:underline'
+                ]"
+              >
+                <span>브리아나 스토커 자세히 알아보기</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="animate-bounce w-3 h-3 ml-1"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
 
           <img
             src="/crypto.svg"
